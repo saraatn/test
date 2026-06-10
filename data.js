3b6946fd6fc66b5af2425f922b1f3e617cd19977
saraatn <saratmw07@gmail.com>
2026-06-10 08:59:22 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,492 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-go-to-station-1",
+      "name": "Go To Station 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.9187878581729265,
+        "pitch": 0.3165437028720106,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3607726706367043,
+          "pitch": 0.575271838059594,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.7882284568284366,
+          "pitch": 0.35269947965878856,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9692191558859236,
+          "pitch": 0.3344836674719627,
+          "title": "Internet of Things",
+          "text": "https://www.youtube.com/watch?v=2IVgKpI4xuU"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -1.6062902727766524,
+        "pitch": 0.2621656497115641,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9906044585238973,
+          "pitch": 0.5367769718270488,
+          "rotation": 0,
+          "target": "0-go-to-station-1"
+        },
+        {
+          "yaw": -1.6219197642372443,
+          "pitch": 0.49832085254709924,
+          "rotation": 0,
+          "target": "2-go-to-station-2"
+        },
+        {
+          "yaw": -0.16637412007596986,
+          "pitch": 0.4070839792073855,
+          "rotation": 0,
+          "target": "9-6-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-go-to-station-2",
+      "name": "Go To Station 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.807215302509106,
+        "pitch": 0.2516224816023822,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6737981906097996,
+          "pitch": 0.5335121945777672,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.7543618371851375,
+          "pitch": 0.49855897189223697,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.921632629797138,
+          "pitch": 0.19916000965580594,
+          "title": "Sorting Robots - Libiao Robotics",
+          "text": "<span style=\"font-size: 16px;\">https://www.youtube.com/watch?v=A1MJwfR3avk</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -2.206741595562834,
+        "pitch": 0.029174715043513544,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9291814435725136,
+          "pitch": 0.46735774740215064,
+          "rotation": 0,
+          "target": "4-go-to-station-3"
+        },
+        {
+          "yaw": 1.3889731746174476,
+          "pitch": 0.6691985969421843,
+          "rotation": 0,
+          "target": "2-go-to-station-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-go-to-station-3",
+      "name": "Go To Station 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.639963208840281,
+        "pitch": -0.24142556711188057,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.792591510735397,
+          "pitch": 0.6888885744645705,
+          "rotation": 0,
+          "target": "5-go-to-station-4"
+        },
+        {
+          "yaw": 0.8937882386317604,
+          "pitch": 0.6239429988084098,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6454256314907516,
+          "pitch": -0.14350918546853997,
+          "title": "Automated Tote Racking System - Linde",
+          "text": "https://www.youtube.com/watch?v=iFvIGe506KA"
+        }
+      ]
+    },
+    {
+      "id": "5-go-to-station-4",
+      "name": "Go To Station 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.8014676085302934,
+        "pitch": -0.09855288065853429,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5723605425528717,
+          "pitch": 0.7431369774993613,
+          "rotation": 0,
+          "target": "6-go-to-station-5"
+        },
+        {
+          "yaw": 1.1456690376655487,
+          "pitch": 0.6704550038988266,
+          "rotation": 0,
+          "target": "4-go-to-station-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7000766080547853,
+          "pitch": 0.06175135915825791,
+          "title": "4 Way Shuttle - ICAS Technology (S) Pte Ltd",
+          "text": "https://www.youtube.com/watch?v=nC7-9PYJ1Tk"
+        }
+      ]
+    },
+    {
+      "id": "6-go-to-station-5",
+      "name": "Go To Station 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.8306948169325654,
+        "pitch": 0.3739042841725819,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.006609759454715,
+          "pitch": 0.44546275918288103,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.29651134259847645,
+          "pitch": 0.5608486540108402,
+          "rotation": 0,
+          "target": "5-go-to-station-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7620155776811437,
+          "pitch": 0.36283380198627313,
+          "title": "GoLEAN",
+          "text": "https://www.youtube.com/watch?v=wfsRAZUnonI"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.8974778182166725,
+        "pitch": 0.26123701584158887,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7996641443419836,
+          "pitch": 0.3469194164205618,
+          "rotation": 0,
+          "target": "8-go-to-station-6"
+        },
+        {
+          "yaw": -0.6420158943587353,
+          "pitch": 0.5437426282384372,
+          "rotation": 0,
+          "target": "6-go-to-station-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-go-to-station-6",
+      "name": "Go To Station 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 3.0621642134747074,
+        "pitch": 0.3248432835465138,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.040995929444369,
+          "pitch": 0.5000174686690322,
+          "rotation": 0,
+          "target": "9-6-7"
+        },
+        {
+          "yaw": 1.0499407607976199,
+          "pitch": 0.5941194075909575,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6287961359912195,
+          "pitch": 0.3371018128848373,
+          "title": "Appsheet",
+          "text": "https://www.youtube.com/watch?v=HOELR0LFNIM"
+        }
+      ]
+    },
+    {
+      "id": "9-6-7",
+      "name": "6-7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 1.311071869824529,
+        "pitch": 0.28818471771301724,
+        "fov": 1.3802197895323118
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4019168544532548,
+          "pitch": 0.6610475162625669,
+          "rotation": 0,
+          "target": "0-go-to-station-1"
+        },
+        {
+          "yaw": -2.8521706596980465,
+          "pitch": 0.6130045558991704,
+          "rotation": 0,
+          "target": "8-go-to-station-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.9749559077015544,
+        "pitch": 0.16413012019780027,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.12230631454325,
+          "pitch": 0.6750051780311974,
+          "rotation": 0,
+          "target": "11-nexus"
+        },
+        {
+          "yaw": 1.4091405367945526,
+          "pitch": 0.5839008072236958,
+          "rotation": 0,
+          "target": "0-go-to-station-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-nexus",
+      "name": "Nexus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 180,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.582300291193608,
+          "pitch": 0.4649935123151945,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.023278412192304643,
+          "pitch": 0.1354448748955086,
+          "title": "Nexus",
+          "text": "https://www.rp.edu.sg/news/highlights/rp-launches-supply-chain-nexus-to-advance-logistics-innovation-and-industry-collaboration/"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
