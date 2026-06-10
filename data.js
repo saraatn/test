2dc96704b068a99f1894afd8408d0e97d13f565f
saraatn <saratmw07@gmail.com>
2026-06-10 09:20:11 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,16 +3,12 @@ var APP_DATA = {
     {
       "id": "0-go-to-station-1",
       "name": "Go To Station 1",
+      "youtubeId": "2IVgKpI4xuU",
+      "videoYaw": 2.9692191558859236,
+      "videoPitch": 0.3344836674719627,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -21,25 +17,15 @@ var APP_DATA = {
         "fov": 1.4637180967878773
       },
       "linkHotspots": [
-        {
-          "yaw": -1.3607726706367043,
-          "pitch": 0.575271838059594,
-          "rotation": 0,
-          "target": "1-"
-        },
-        {
-          "yaw": 0.7882284568284366,
-          "pitch": 0.35269947965878856,
-          "rotation": 0,
-          "target": "10-"
-        }
+        { "yaw": -1.3607726706367043, "pitch": 0.575271838059594, "rotation": 0, "target": "1-" },
+        { "yaw": 0.7882284568284366, "pitch": 0.35269947965878856, "rotation": 0, "target": "10-" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.9692191558859236,
-          "pitch": 0.3344836674719627,
-          "title": "Internet of Things",
-          "text": "https://www.youtube.com/watch?v=2IVgKpI4xuU"
+          "yaw": 2.918,
+          "pitch": 0.1,
+          "title": "Station 1 Information",
+          "text": "Welcome to Station 1. This is the entry overview point of the lab setup."
         }
       ]
     },
@@ -47,15 +33,8 @@ var APP_DATA = {
       "id": "1-",
       "name": "",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -64,40 +43,21 @@ var APP_DATA = {
         "fov": 1.4637180967878773
       },
       "linkHotspots": [
-        {
-          "yaw": 1.9906044585238973,
-          "pitch": 0.5367769718270488,
-          "rotation": 0,
-          "target": "0-go-to-station-1"
-        },
-        {
-          "yaw": -1.6219197642372443,
-          "pitch": 0.49832085254709924,
-          "rotation": 0,
-          "target": "2-go-to-station-2"
-        },
-        {
-          "yaw": -0.16637412007596986,
-          "pitch": 0.4070839792073855,
-          "rotation": 0,
-          "target": "9-6-7"
-        }
+        { "yaw": 1.9906044585238973, "pitch": 0.5367769718270488, "rotation": 0, "target": "0-go-to-station-1" },
+        { "yaw": -1.6219197642372443, "pitch": 0.49832085254709924, "rotation": 0, "target": "2-go-to-station-2" },
+        { "yaw": -0.16637412007596986, "pitch": 0.4070839792073855, "rotation": 0, "target": "9-6-7" }
       ],
       "infoHotspots": []
     },
     {
       "id": "2-go-to-station-2",
       "name": "Go To Station 2",
+      "youtubeId": "A1MJwjR3avk",
+      "videoYaw": 2.921632629797138,
+      "videoPitch": 0.19916000965580594,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -106,25 +66,15 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -1.6737981906097996,
-          "pitch": 0.5335121945777672,
-          "rotation": 0,
-          "target": "3-"
-        },
-        {
-          "yaw": 1.7543618371851375,
-          "pitch": 0.49855897189223697,
-          "rotation": 0,
-          "target": "1-"
-        }
+        { "yaw": -1.6737981906097996, "pitch": 0.5335121945777672, "rotation": 0, "target": "3-" },
+        { "yaw": 1.7543618371851375, "pitch": 0.49855897189223697, "rotation": 0, "target": "1-" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.921632629797138,
-          "pitch": 0.19916000965580594,
-          "title": "Sorting Robots - Libiao Robotics",
-          "text": "<span style=\"font-size: 16px;\">https://www.youtube.com/watch?v=A1MJwfR3avk</span>"
+          "yaw": 2.807,
+          "pitch": 0.1,
+          "title": "Station 2 Information",
+          "text": "Details and processing metrics for Station 2 are managed here."
         }
       ]
     },
@@ -132,15 +82,8 @@ var APP_DATA = {
       "id": "3-",
       "name": "",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -149,34 +92,20 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -1.9291814435725136,
-          "pitch": 0.46735774740215064,
-          "rotation": 0,
-          "target": "4-go-to-station-3"
-        },
-        {
-          "yaw": 1.3889731746174476,
-          "pitch": 0.6691985969421843,
-          "rotation": 0,
-          "target": "2-go-to-station-2"
-        }
+        { "yaw": -1.9291814435725136, "pitch": 0.46735774740215064, "rotation": 0, "target": "4-go-to-station-3" },
+        { "yaw": 1.3889731746174476, "pitch": 0.6691985969421843, "rotation": 0, "target": "2-go-to-station-2" }
       ],
       "infoHotspots": []
     },
     {
       "id": "4-go-to-station-3",
       "name": "Go To Station 3",
+      "youtubeId": "iFvlGe506KA",
+      "videoYaw": 2.6454256314907516,
+      "videoPitch": -0.14350918546853997,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -185,41 +114,27 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -1.792591510735397,
-          "pitch": 0.6888885744645705,
-          "rotation": 0,
-          "target": "5-go-to-station-4"
-        },
-        {
-          "yaw": 0.8937882386317604,
-          "pitch": 0.6239429988084098,
-          "rotation": 0,
-          "target": "3-"
-        }
+        { "yaw": -1.792591510735397, "pitch": 0.6888885744645705, "rotation": 0, "target": "5-go-to-station-4" },
+        { "yaw": 0.8937882386317604, "pitch": 0.6239429988084098, "rotation": 0, "target": "3-" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.6454256314907516,
-          "pitch": -0.14350918546853997,
-          "title": "Automated Tote Racking System - Linde",
-          "text": "https://www.youtube.com/watch?v=iFvIGe506KA"
+          "yaw": 2.639,
+          "pitch": -0.1,
+          "title": "Station 3 Information",
+          "text": "Overview of operations and standard protocols at Station 3."
         }
       ]
     },
     {
       "id": "5-go-to-station-4",
       "name": "Go To Station 4",
+      "youtubeId": "nC7-9PYJ1Tk",
+      "videoYaw": 2.7000766080547853,
+      "videoPitch": 0.06175135915825791,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -228,41 +143,27 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -0.5723605425528717,
-          "pitch": 0.7431369774993613,
-          "rotation": 0,
-          "target": "6-go-to-station-5"
-        },
-        {
-          "yaw": 1.1456690376655487,
-          "pitch": 0.6704550038988266,
-          "rotation": 0,
-          "target": "4-go-to-station-3"
-        }
+        { "yaw": -0.5723605425528717, "pitch": 0.7431369774993613, "rotation": 0, "target": "6-go-to-station-5" },
+        { "yaw": 1.1456690376655487, "pitch": 0.6704550038988266, "rotation": 0, "target": "4-go-to-station-3" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.7000766080547853,
-          "pitch": 0.06175135915825791,
-          "title": "4 Way Shuttle - ICAS Technology (S) Pte Ltd",
-          "text": "https://www.youtube.com/watch?v=nC7-9PYJ1Tk"
+          "yaw": 2.801,
+          "pitch": 0.0,
+          "title": "Station 4 Information",
+          "text": "Inventory logistics and scanning workflow monitored at Station 4."
         }
       ]
     },
     {
       "id": "6-go-to-station-5",
       "name": "Go To Station 5",
+      "youtubeId": "wjfRAZUnonI",
+      "videoYaw": 2.7620155776811437,
+      "videoPitch": 0.36283380198627313,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -271,25 +172,15 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -2.006609759454715,
-          "pitch": 0.44546275918288103,
-          "rotation": 0,
-          "target": "7-"
-        },
-        {
-          "yaw": -0.29651134259847645,
-          "pitch": 0.5608486540108402,
-          "rotation": 0,
-          "target": "5-go-to-station-4"
-        }
+        { "yaw": -2.006609759454715, "pitch": 0.44546275918288103, "rotation": 0, "target": "7-" },
+        { "yaw": -0.29651134259847645, "pitch": 0.5608486540108402, "rotation": 0, "target": "5-go-to-station-4" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.7620155776811437,
-          "pitch": 0.36283380198627313,
-          "title": "GoLEAN",
-          "text": "https://www.youtube.com/watch?v=wfsRAZUnonI"
+          "yaw": 2.830,
+          "pitch": 0.2,
+          "title": "Station 5 Information",
+          "text": "Quality assurance checking area configuration parameters."
         }
       ]
     },
@@ -297,15 +188,8 @@ var APP_DATA = {
       "id": "7-",
       "name": "",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -314,34 +198,20 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": 2.7996641443419836,
-          "pitch": 0.3469194164205618,
-          "rotation": 0,
-          "target": "8-go-to-station-6"
-        },
-        {
-          "yaw": -0.6420158943587353,
-          "pitch": 0.5437426282384372,
-          "rotation": 0,
-          "target": "6-go-to-station-5"
-        }
+        { "yaw": 2.7996641443419836, "pitch": 0.3469194164205618, "rotation": 0, "target": "8-go-to-station-6" },
+        { "yaw": -0.6420158943587353, "pitch": 0.5437426282384372, "rotation": 0, "target": "6-go-to-station-5" }
       ],
       "infoHotspots": []
     },
     {
       "id": "8-go-to-station-6",
       "name": "Go To Station 6",
+      "youtubeId": "HOELR0LFNIM",
+      "videoYaw": 2.6287961359912195,
+      "videoPitch": 0.3371018128848373,
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -350,41 +220,24 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": -2.040995929444369,
-          "pitch": 0.5000174686690322,
-          "rotation": 0,
-          "target": "9-6-7"
-        },
-        {
-          "yaw": 1.0499407607976199,
-          "pitch": 0.5941194075909575,
-          "rotation": 0,
-          "target": "7-"
-        }
+        { "yaw": -2.040995929444369, "pitch": 0.5000174686690322, "rotation": 0, "target": "9-6-7" },
+        { "yaw": 1.0499407607976199, "pitch": 0.5941194075909575, "rotation": 0, "target": "7-" }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.6287961359912195,
-          "pitch": 0.3371018128848373,
-          "title": "Appsheet",
-          "text": "https://www.youtube.com/watch?v=HOELR0LFNIM"
+          "yaw": 3.062,
+          "pitch": 0.2,
+          "title": "Station 6 Information",
+          "text": "Final integration loop system prior to central hub delivery."
         }
       ]
     },
     {
       "id": "9-6-7",
-      "name": "6-7",
+      "name": "Transition 4", 
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -393,34 +246,17 @@ var APP_DATA = {
         "fov": 1.3802197895323118
       },
       "linkHotspots": [
-        {
-          "yaw": 1.4019168544532548,
-          "pitch": 0.6610475162625669,
-          "rotation": 0,
-          "target": "0-go-to-station-1"
-        },
-        {
-          "yaw": -2.8521706596980465,
-          "pitch": 0.6130045558991704,
-          "rotation": 0,
-          "target": "8-go-to-station-6"
-        }
+        { "yaw": 1.4019168544532548, "pitch": 0.6610475162625669, "rotation": 0, "target": "0-go-to-station-1" },
+        { "yaw": -2.8521706596980465, "pitch": 0.6130045558991704, "rotation": 0, "target": "8-go-to-station-6" }
       ],
       "infoHotspots": []
     },
     {
       "id": "10-",
-      "name": "",
+      "name": "Transition 5", 
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 400,
       "initialViewParameters": {
@@ -429,18 +265,8 @@ var APP_DATA = {
         "fov": 1.4637180967878773
       },
       "linkHotspots": [
-        {
-          "yaw": 3.12230631454325,
-          "pitch": 0.6750051780311974,
-          "rotation": 0,
-          "target": "11-nexus"
-        },
-        {
-          "yaw": 1.4091405367945526,
-          "pitch": 0.5839008072236958,
-          "rotation": 0,
-          "target": "0-go-to-station-1"
-        }
+        { "yaw": 3.12230631454325, "pitch": 0.6750051780311974, "rotation": 0, "target": "11-nexus" },
+        { "yaw": 1.4091405367945526, "pitch": 0.5839008072236958, "rotation": 0, "target": "0-go-to-station-1" }
       ],
       "infoHotspots": []
     },
@@ -448,15 +274,8 @@ var APP_DATA = {
       "id": "11-nexus",
       "name": "Nexus",
       "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
+        { "tileSize": 256, "size": 256, "fallbackOnly": true },
+        { "tileSize": 512, "size": 512 }
       ],
       "faceSize": 180,
       "initialViewParameters": {
@@ -465,12 +284,7 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
-        {
-          "yaw": -1.582300291193608,
-          "pitch": 0.4649935123151945,
-          "rotation": 0,
-          "target": "10-"
-        }
+        { "yaw": -1.582300291193608, "pitch": 0.4649935123151945, "rotation": 0, "target": "10-" }
       ],
       "infoHotspots": [
         {
